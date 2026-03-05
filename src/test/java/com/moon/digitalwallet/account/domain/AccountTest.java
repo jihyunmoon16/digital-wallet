1e7fe20c51c10856f259643d2d7690f5689b5608
jihyun <sugaryfood@naver.com>
2026-03-05 10:06:14 +0900

    test(account): consolidate domain tests under account package

--- src/test/java/com/moon/digitalwallet/account/domain/AccountTest.java
+++ src/test/java/com/moon/digitalwallet/account/domain/AccountTest.java
@@ -11,8 +11,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class AccountTest {
 
     @Test
-    void withdraw_rejectsNonPositiveAmount() {
+    void deposit_rejectsNonPositiveAmount() {
+        Account account = new Account(new User("tester"));
 
+        assertThatThrownBy(() -> account.deposit(BigDecimal.ZERO)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> account.deposit(new BigDecimal("-1.00"))).isInstanceOf(IllegalArgumentException.class);
+
+        assertThat(account.getBalance()).isEqualByComparingTo("0.00");
+    }
+
+    @Test
+    void withdraw_rejectsNonPositiveAmount() {
         Account account = new Account(new User("tester"));
 
         assertThatThrownBy(() -> account.withdraw(BigDecimal.ZERO)).isInstanceOf(IllegalArgumentException.class);
