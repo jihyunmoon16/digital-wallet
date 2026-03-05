5ee3b9ea7387ef1cd9be20f1d8a7631162224872
jihyun <sugaryfood@naver.com>
2026-03-05 14:44:47 +0900

    test(config): add dedicated test profile database settings

--- src/test/java/com/moon/digitalwallet/DigitalWalletApplicationTests.java
+++ src/test/java/com/moon/digitalwallet/DigitalWalletApplicationTests.java
@@ -2,8 +2,10 @@ package com.moon.digitalwallet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DigitalWalletApplicationTests {
 
     @Test
