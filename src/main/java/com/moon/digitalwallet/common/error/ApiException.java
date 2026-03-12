661c1e006dab294940a86aae683ace9f6227a8dd
jihyun <sugaryfood@naver.com>
2026-03-12 14:03:58 +0900

    refactor(error): remove deprecated ApiException

--- src/main/java/com/moon/digitalwallet/common/error/ApiException.java
+++
@@ -1,9 +0,0 @@
-package com.moon.digitalwallet.common.error;
-
-@Deprecated
-public class ApiException extends BusinessException {
-
-    public ApiException(ErrorCode errorCode, String message) {
-        super(errorCode, message);
-    }
-}
